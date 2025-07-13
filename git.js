0bcb2cdcc11cb8a3da8ad08dc132592cdbdcb2f5
pshingsl <w455ok@gmail.com>
2025-07-13 22:52:46 +0900

    기초 다지기

--- git.js
+++ git.js
@@ -1,6 +1,7 @@
-const test= 'git'
-console.log(test)
+// 입력 “숫자 배열 [3, 7, 12, 5, 20]
+// 출력: 10보다 큰 숫자
+// 조건: 새로운 배열
 
-// commit
-const arr =[1,2,3,4,5]
-console.log(arr)
+const arr = [3, 7, 12, 5, 20]
+const result = arr.filter(a => a>10)
+console.log(result)
